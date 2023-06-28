3dbc046658f5461af607521ca0c933ad45f766ec
Gioele Iacovella <g.ioele96@outlook.it>
2023-06-28 17:29:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'gioele',
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>LA MIA PRIMA APP CON VITE</h1>
+</template>
+
+<style>
+h1 {
+  text-align: center;
+  margin-top: 10px;
+  color: red;
+}
+</style>
